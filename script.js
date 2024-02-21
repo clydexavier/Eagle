f3f6f54f51a3a79ccbe47596c6ea02748b0cebd9
saabyer <salarclydexavier@gmail.com>
2024-02-22 00:45:26 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirectToSignup() {
+    // Set the URL of your signup page
+    var signupPageUrl = "signup.html"; // Replace with the actual path
+
+    // Redirect to the signup page
+    window.location.href = signupPageUrl;
+  }
